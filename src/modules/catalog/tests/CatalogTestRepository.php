ba39539402569ef101f06d9c9061b048d56c2418
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-01 14:22:57 +0200

    create CatalogTestRepository

---
+++ src/modules/catalog/tests/CatalogTestRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DDD\Modules\Catalog\Tests;
+
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Category\CountCategoriesRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Category\CreateCategoryRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Category\ReadCategoriesRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Category\ReadCategoryByIdRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Category\ReadCategoryByNameRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Product\CountProductsRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Product\CreateProductRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Product\ReadProductsByCategoryIdRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Product\ReadProductsRepo;
+
+abstract class CatalogTestRepository implements
+    ReadCategoryByNameRepo,
+    CreateCategoryRepo,
+    CreateProductRepo,
+    ReadCategoryByIdRepo,
+    ReadCategoriesRepo,
+    CountCategoriesRepo,
+    CountProductsRepo,
+    ReadProductsByCategoryIdRepo,
+    ReadProductsRepo
+{}
