ad8799fec9364c93e444a42d11d67db8d20417f1
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-01 14:09:31 +0200

    use static factory methods in catalog entities

--- src/modules/catalog/business/entities/Category.php
+++ src/modules/catalog/business/entities/Category.php
@@ -2,12 +2,21 @@
 
 namespace DDD\Modules\Catalog\Business\Entities;
 
-final class Category
+final readonly class Category
 {
-    public string $id;
-
-    public function __construct(
+    private function __construct(
+        public ?string $id,
         public string $name,
         public string $description,
     ) {}
+
+    public static function createWihoutId(string $name, string $description): self
+    {
+        return new self(null, $name, $description);
+    }
+
+    public static function createWithId(string $id, string $name, string $description): self
+    {
+        return new self($id, $name, $description);
+    }
 }
--- src/modules/catalog/business/entities/Product.php
+++ src/modules/catalog/business/entities/Product.php
@@ -2,13 +2,22 @@
 
 namespace DDD\Modules\Catalog\Business\Entities;
 
-final class Product
+final readonly class Product
 {
-    public string $id;
-    
-    public function __construct(
+    private function __construct(
+        public ?string $id,
         public string $name,
         public float $price,
         public Category $category,
     ) {}
+
+    public static function createWithoutId(string $name, float $price, Category $category): self
+    {
+        return new self(null, $name, $price, $category);
+    }
+
+    public static function createWith(string $id, string $name, float $price, Category $category): self
+    {
+        return new self($id, $name, $price, $category);
+    }
 }
