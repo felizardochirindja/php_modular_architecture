16e88217143e91787f452676f6f647398a017ea8
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-01 14:32:50 +0200

    feat: create add cart item action

---
+++ src/modules/customer/business/app/actions/addCartItem/AddCartItemAction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DDD\Modules\Customer\Business\App\Actions\AddCartItem;
+
+use DDD\Modules\Customer\Business\App\Ports\Repo\Cart\CreateCartItemRepo;
+use DDD\Modules\Customer\Business\App\Ports\Repo\Cart\GetCartByIdRepo;
+use DDD\Modules\Customer\Business\App\Ports\Repo\Cart\GetItemByIdRepo;
+use DomainException;
+
+final class AddCartItemAction
+{
+    public function __construct(
+        private GetCartByIdRepo | GetItemByIdRepo | CreateCartItemRepo $repo,
+    ) {}
+
+    public function execute(int $itemId, int $cartId): mixed
+    {
+        $item = $this->repo->getItemById($itemId);
+
+        if (!$item) {
+            throw new DomainException("item does not exist");
+        }
+
+        $cart = $this->repo->getCartByIdRepo($cartId);
+
+        if (!$cart) {
+            throw new DomainException("cart does not exist");
+        }
+
+        $this->repo->createCartItem($cartId, $itemId);
+    }
+}
