28a856aee9b4a26188a338c913414b4f4ecb983a
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-01 14:24:45 +0200

    test show products action

---
+++ src/modules/catalog/tests/business/app/actions/ShowProductsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use DDD\Modules\Catalog\Business\App\Actions\ShowProducts\ShowProductsAction;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Product\CountProductsRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Product\ReadProductsRepo;
+use DDD\Modules\Catalog\Tests\CatalogTestRepository;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+final class ShowProductsTest extends TestCase
+{
+    /** @test */
+    public function itShouldShowProducts(): void
+    {
+        /** @var ReadProductsRepo|CountProductsRepo|MockObject $repo */
+        $repo = $this->createStub(CatalogTestRepository::class);
+
+        $repo
+            ->method("countProducts")
+            ->willReturn(1);
+
+        $expectedProducts = [
+            [
+                'id' => '1',
+                'name' => 'product',
+                'description' => 'description',
+            ]
+        ];
+
+        $repo
+            ->method("readProducts")
+            ->willReturn($expectedProducts);
+
+        $action = new ShowProductsAction($repo);
+
+        $products = $action->execute(1, 5);
+
+        $this->assertEquals($expectedProducts, $products);
+    }
+}
