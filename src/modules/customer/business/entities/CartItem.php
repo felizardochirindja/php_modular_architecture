32adf484094e12aafa4d6e734290fee4117f0727
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-01 16:02:39 +0200

    feat: use static factory method to create CartItem objects

--- src/modules/customer/business/entities/CartItem.php
+++ src/modules/customer/business/entities/CartItem.php
@@ -2,14 +2,23 @@
 
 namespace DDD\Modules\Customer\Business\Entities;
 
-final class CartItem
+final readonly class CartItem
 {
-    public string $id;
-
-    public function __construct(
+    private function __construct(
+        public ?string $id,
         public string $name,
         public float $price,
         public int $categoryId,
         public int $quantity,
     ) {}
+
+    public static function createWithoutId(string $name, float $price, string $categoryId, int $quantity): self
+    {
+        return new self(null, $name, $price, $categoryId, $quantity);
+    }
+
+    public static function createWithId(string $id, string $name, float $price, string $categoryId, int $quantity): self
+    {
+        return new self($id, $name, $price, $categoryId, $quantity);
+    }
 }
