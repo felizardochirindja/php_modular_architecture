f49948bcf4d6c52f462be55b95d7fac2ceb43417
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-01 14:24:23 +0200

    test show product by category action

---
+++ src/modules/catalog/tests/business/app/actions/ShowProductsByCategoryTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use DDD\Modules\Catalog\Business\App\Actions\showProductsByCategory\ShowProductsByCategoryAction;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Category\ReadCategoryByIdRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Product\CountProductsRepo;
+use DDD\Modules\Catalog\Business\App\Ports\Repo\Product\ReadProductsByCategoryIdRepo;
+use DDD\Modules\Catalog\Business\Entities\Category;
+use DDD\Modules\Catalog\Tests\CatalogTestRepository;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class ShowProductsByCategoryTest extends TestCase
+{
+    /** @test */
+    public function itShouldShowProductsByCategory(): void
+    {
+        /** @var ReadCategoryByIdRepo|CountProductsRepo|ReadProductsByCategoryIdRepo|MockObject $repo */
+        $repo = $this->createStub(CatalogTestRepository::class);
+
+        $repo
+            ->method('countProducts')
+            ->willReturn(1);
+
+        $category = Category::createWithId('1', 'category', 'description');
+
+        $repo
+            ->method('readCategoryById')
+            ->willReturn($category);
+
+        $expectedProducts = [
+            [
+                'id' => '1',
+                'name' => 'product',
+                'description' => 'description',
+            ]
+        ];
+
+        $repo
+            ->method('readProductsByCategoryId')
+            ->willReturn($expectedProducts);
+
+        $action = new ShowProductsByCategoryAction($repo);
+
+        $products = $action->execute(1, 1, 5);
+
+        $this->assertSame($expectedProducts, $products);
+    }
+
+    /** @test */
+    public function itShouldThrowExceptionIfCategoryDoesNotExist(): void
+    {
+        /** @var ReadCategoryByIdRepo|CountProductsRepo|MockObject $repo */
+        $repo = $this->createStub(CatalogTestRepository::class);
+
+        $repo
+            ->method('countProducts')
+            ->willReturn(1);
+
+        $repo
+            ->method('readCategoryById')
+            ->willReturn(null);
+
+        $action = new ShowProductsByCategoryAction($repo);
+
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('category with id of 1 does not exist!');
+
+        $action->execute(1, 1, 5);
+    }
+}
