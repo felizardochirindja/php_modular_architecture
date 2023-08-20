5aed1c94c7c8e0005d20effba44761e5e9fea071
felizardochirindja <felizardo.chirindja@gmail.com>
2023-08-20 04:56:42 +0200

    ref: change price type from int to float

--- src/modules/catalog/business/types/ProductPrice.php
+++ src/modules/catalog/business/types/ProductPrice.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace DDD\Modules\Catalog\Business\Price;
+namespace DDD\Modules\Catalog\Business\Types;
 
 use DomainException;
 
 final readonly class ProductPrice
 {
     public function __construct(
-        public int $price
+        public float $price
     ) {
         if ($price < 0) {
             throw new DomainException("price must be greater than zero");
